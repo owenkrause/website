34ccb98df71921952ac362c780fc0bcdf4dd9cb8
owenkrause <owenjkrause@gmail.com>
2026-04-01 04:58:18 -0700

    v2 or something

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
